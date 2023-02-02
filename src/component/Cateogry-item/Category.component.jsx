8623174107569dbbdda24daa4e01407fa31b395d
ramdeniakki <ramdeniakki35@gmail.com>
2023-02-02 19:01:57 +0530

    Updating Directory Page and Category Page

---
+++ src/component/Cateogry-item/Category.component.jsx
@@ -0,0 +1,17 @@
+import './Category.style.scss'
+
+const CategoryItems = ( ({Category}) =>{
+    const {imageUrl,title} = Category;
+    return(
+        <div className='category-container'>
+           <div className='background-image'
+           style={{backgroundImage:`url(${imageUrl})`,
+        }}
+         />
+         <div className='category-body-container' >
+          <h2>{title}</h2>
+          <p>Shop now</p>
+         </div>        
+        </div>)
+})
+export default CategoryItems
